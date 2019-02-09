b4d607263651ee8fa834c68157e64c804ae49d6d
sick-clim <you@example.com>
2019-02-10 01:46:29 +0900

    Add apt repository

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ set -e
 readonly DEV_PATH=$HOME
 
 if ! which ansible; then
+    sudo apt-add-repository ppa:ansible/ansible -y
     sudo apt update && sudo apt upgrade -y
-    sudo apt install ansible
+    sudo apt install -y ansible
 fi
 
 git clone https://github.com/sick-clim/ansible-local-env.git $DEV_PATH/src/github.com/sick-clim/ansible-local-env
