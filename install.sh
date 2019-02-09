47a8ec8c61ad049d15bc1604441b147115ce4610
sick-clim <you@example.com>
2019-02-09 13:38:56 +0900

    Change local path

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
-readonly DEV_PATH=$HOME/dev
+readonly DEV_PATH=$HOME
 
 if ! which ansible; then
     sudo apt update && sudo apt upgrade -y
     sudo apt install ansible
 fi
 
-git clone https://github.com/sick-clim/ansible-local-env.git $DEV_PATH/github.com/sick-clim/ansible-local-env
+git clone https://github.com/sick-clim/ansible-local-env.git $DEV_PATH/src/github.com/sick-clim/ansible-local-env
 
 cd $DEV_PATH/github.com/sick-clim/ansible-local-env
 ansible-playbook local-devenv.yml
+
